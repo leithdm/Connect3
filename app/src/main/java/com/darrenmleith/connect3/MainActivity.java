124e74d5b2358bb3587b361e9fafaba32979d969
leithdm@gmail.com <darrenm.leith@lethalapps.com>
2020-04-16 21:37:22 +0100

    1. dropIn() now applies to all ImageViews. Removed default image for each ImageView and made them all use onClick: dropIn()

--- app/src/main/java/com/darrenmleith/connect3/MainActivity.java
+++ app/src/main/java/com/darrenmleith/connect3/MainActivity.java
@@ -3,6 +3,9 @@ package com.darrenmleith.connect3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void dropIn(View view) {
+        Log.i("button", "imagePressed");
+        ImageView counter = (ImageView) view;
+        counter.setTranslationY(-2000);
+        counter.setImageResource(R.drawable.red);
+        counter.animate().translationYBy(2000).setDuration(300);
     }
 }
